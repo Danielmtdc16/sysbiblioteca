9cf685cda364bceb6481c1ceedda744f7f34f418
Madson_Silva <madsonsilva2005@gmail.com>
2022-10-07 17:24:20 -0300

    Implementado calculo de dias de reserva

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def dias_uteis(data_inicial, data_final)
+		dias = (data_final-data_inicial+1).to_i
+		domingos = (data_inicial..data_final).group_by(&:wday)[0]
+		sabados = (data_inicial..data_final).group_by(&:wday)[6]
+		@diasuteis = dias-(domingos != nil ? domingos.length : 0)-(sabados != nil ? sabados.length : 0)
+	end
 end
--- app/models/livro_leitor.rb
+++ app/models/livro_leitor.rb
@@ -1,3 +1,4 @@
+require "date"
 class LivroLeitor < ApplicationRecord
   belongs_to :user
   belongs_to :livro
@@ -7,13 +8,32 @@ class LivroLeitor < ApplicationRecord
 
   validate :validate_update_reserva, on: :update
 
+  before_create :salve_reserva
+
+  def salve_reserva
+    configuracao = ConfiguracoesSistema.find(1)
+    self.data_possivel_entrega = Date.current + configuracao.prazo_dias.day
+    dias = configuracao.prazo_dias
+    data_final = Date.current + configuracao.prazo_dias.day
+    data_inicial = Date.current
+    begin
+      dias = dias+ (dias - dias_uteis(data_inicial, (Date.current + dias)))
+    end while configuracao.prazo_dias>=dias
+    puts "dias:"+dias.to_s
+    self.data_possivel_entrega = (Date.current + dias)
+
+  end
+
+
   def validate_update_reserva
     if self.status == "Entregue" and self.data_entrega == nil
-        errors.add(:data_entrega, ": para status de entregue a data de entrega é obrigatória")
+        errors.add(:data_entrega, ": é obrigatória para status de entregue")
     end
     if (self.data_inicio != nil and self.data_entrega != nil) and 
       self.data_inicio > self.data_entrega
-      errors.add(:data_entrega, ": para não pode ser menor que data inicio")
+      errors.add(:data_entrega, ": não pode ser menor que data inicio")
     end
   end 
+
+
 end
