16ec58b9a9c9878cf63b69e481cbc020d0ddc397
Madson_Silva <madsonsilva2005@gmail.com>
2022-10-07 15:22:34 -0300

    consertado os erros de salvar varios autores

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,4 +9,5 @@ require("@rails/ujs").start();
 require("turbolinks").start()
 require("@rails/activestorage").start();
 require("channels");
+require("jquery");
 require("@nathanvda/cocoon").start();
